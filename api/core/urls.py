c24acc35ab3209938c91aa8d445edb720476c5fc
alepalacio <g17ada03@hackademy.mx>
2022-04-13 03:29:13 -0300

    created SignUp and LoginView.  Tokens generated for authentication.

--- api/core/urls.py
+++ api/core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls'),),
+    path('accounts/', include('accounts.urls'),),
 ]
--- api/users/urls.py
+++ api/users/urls.py
@@ -3,4 +3,5 @@ from users import views
 
 urlpatterns = [
     path('signup/', views.UserSignUpView.as_view(),),
+    path('login/', views.UserLoginView.as_view(),),
 ]
